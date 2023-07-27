507f003aca2a61d01701d8ea02f7d8e0367091ff
SEVIN <1213hope@gmail.com>
2023-07-27 13:39:18 +0900

    no message

--- js/button.js
+++ js/button.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
       var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
       if (isIOS) {
-        window.location.href = 'https://apps.apple.com/us/app/앱이름/id넘버';
+        window.location.href = 'https://apps.apple.com/us/app/앱이름/id6452395655';
       } else {
-        window.location.href = 'https://play.google.com/store/apps/details?id=앱주소'; 
+        window.location.href = 'https://play.google.com/store/apps/details?id=com.atowner.hairbe'; 
       }
     }
   });
