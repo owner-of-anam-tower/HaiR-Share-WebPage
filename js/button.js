e9f895b0313bb86ca618ff517e501881caf37383
SEVIN <1213hope@gmail.com>
2023-07-28 16:25:16 +0900

    no message

--- js/button.js
+++ js/button.js
@@ -1,3 +1,12 @@
+var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+if (isIOS) {
+  window.location.href = 'https://apps.apple.com/us/app/앱이름/id6452395655';
+} else {
+  window.location.href = 'https://play.google.com/store/apps/details?id=com.atowner.hairbe'; 
+}
+
+
 $(document).ready(function() {
     $('#redirectButton').click(function() {
       redirectToStore();
